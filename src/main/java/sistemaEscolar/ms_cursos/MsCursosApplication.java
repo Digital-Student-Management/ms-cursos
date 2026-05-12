8e9ecf01f763336551aa727524fe74645b4c2fa3
BenjaminVIs <bg6678522@gmail.com>
2026-05-12 19:02:17 -0400

    first commit

---
+++ src/main/java/sistemaEscolar/ms_cursos/MsCursosApplication.java
@@ -0,0 +1,13 @@
+package sistemaEscolar.ms_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCursosApplication.class, args);
+	}
+
+}
